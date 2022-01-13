b4c5a593d415163f9768850b8b2885591cf59fe9
Parth Panchal <panchalparth658@gmail.com>
2022-01-13 14:10:42 -0500

    Task Completed

--- Main.java
+++ Main.java
@@ -1,5 +1,49 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		// String
+		// String name = "Parth";
+		// System.out.println(name);
+
+		// // integer
+		// final int age = 20;
+		// System.out.println(age);
+
+		// // println
+		// System.out.println("Name: " + name);
+		// System.out.println("Age: " + age);
+
+		// Add
+		int a = 10, b = 5;
+
+		// System.out.println(a + b);
+		// System.out.println(a - b);
+		// System.out.println("Div: " + a / b);
+		// System.out.println("Mul: " + a * b);
+		// System.out.println("Mod: " + a % b);
+
+		// // double and float
+		// float myNum = 5.75f;
+		// System.out.println(myNum);
+
+		// double myNum1 = 19.99d;
+		// System.out.println(myNum1);
+
+		// // boolean
+		// boolean isJavaFun = false;
+		// System.out.println(isJavaFun);
+
+		// System.out.println(isJavaFun);
+
+		// char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+		// System.out.println(myVar1);
+		// System.out.println(myVar2);
+		// System.out.println(myVar3);
+
+		int i = 0;
+		while (i < 5) {
+			System.out.println(i);
+			i++;
+		}
+
 	}
 }
